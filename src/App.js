933bbef14811d98479a9f570ec8ecb03ee3649e6
Yoonheecheol <luk2903201@gmail.com>
2021-12-20 14:13:16 +0900

    win-test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
-  return <div>Home 페이지 입니다.</div>;
+  return <div>(win-test branchHome) 페이지 입니다.</div>;
 }
 
 export default App;
